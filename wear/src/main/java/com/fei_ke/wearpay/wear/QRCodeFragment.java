6dbc87ebb555f141d5ac097f3c7c1977604c11e0
fei-ke <jinyang656@qq.com>
2015-10-03 15:02:37 +0800

    show a progress bar

--- wear/src/main/java/com/fei_ke/wearpay/wear/QRCodeFragment.java
+++ wear/src/main/java/com/fei_ke/wearpay/wear/QRCodeFragment.java
@@ -8,22 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 /**
  * Created by fei-ke on 2015/9/28.
  */
 public class QRCodeFragment extends Fragment {
     ImageView imageView;
+    ProgressBar progressBar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        imageView = new ImageView(getActivity());
-        imageView.setImageResource(R.mipmap.ic_launcher);
-        return imageView;
+        View rootView = inflater.inflate(R.layout.fragment_code, container, false);
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+        return rootView;
     }
 
     public void setCodeImage(Bitmap codeImage) {
+        if (progressBar.getVisibility() == View.VISIBLE) {
+            progressBar.setVisibility(View.GONE);
+        }
         imageView.setImageBitmap(codeImage);
     }
 }

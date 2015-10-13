c4883f9eaa347ff9ffa278877c0e28e09c94aad3
love999 <abcd123>
2015-10-13 11:25:14 +0800

    1.支付宝支付时唤醒屏幕 2.检测到支付宝未运行时先启动支付宝

--- app/src/main/java/com/fei_ke/wearpay/MainActivity.java
+++ app/src/main/java/com/fei_ke/wearpay/MainActivity.java
@@ -1,11 +1,23 @@
 package com.fei_ke.wearpay;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.fei_ke.wearpay.commen.Constans;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
@@ -28,8 +40,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.alipay).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
-                sendBroadcast(intent);
+                startAlipay();
             }
         });
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
@@ -38,6 +49,77 @@ public class MainActivity extends Activity {
             }
         });
         startService(new Intent(this, WatchServices.class));
+    }
 
+    void startAlipay(){
+        Log.i("tag",isRunning()+"");
+        if(isRunning()){
+            Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
+            sendBroadcast(intent);
+        }else{
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    doStartApplicationWithPackageName(Constans.ALIPAY_PACKAGE);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    startAlipay();
+                }
+            }).start();
+        }
+    }
+
+    public boolean isRunning(){
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo rsi : am.getRunningServices(Integer.MAX_VALUE)) {
+
+            String pkgName = rsi.service.getPackageName();
+            if(pkgName.equals(Constans.ALIPAY_PACKAGE)){
+                if(rsi.process.equals(Constans.ALIPAY_PACKAGE)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void doStartApplicationWithPackageName(String packagename) {
+
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        List<ResolveInfo> resolveinfoList = getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packagename = ����packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // ����ComponentName����1:packagename����2:MainActivity·��
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/fei_ke/wearpay/WatchServices.java
+++ app/src/main/java/com/fei_ke/wearpay/WatchServices.java
@@ -1,11 +1,19 @@
 package com.fei_ke.wearpay;
 
+import android.app.ActivityManager;
+import android.app.KeyguardManager;
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
 
 import com.fei_ke.wearpay.commen.Constans;
@@ -22,6 +30,7 @@ import com.google.android.gms.wearable.Wearable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import hugo.weaving.DebugLog;
 
@@ -147,9 +156,95 @@ public class WatchServices extends WearService {
     }
 
     public void launchAlipayWallet() {
-        Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
-        intent.setPackage(Constans.ALIPAY_PACKAGE);
-        sendBroadcast(intent);
+        wakeUpAndUnlock(this);
+
+        if(isRunning()){
+            Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
+            intent.setPackage(Constans.ALIPAY_PACKAGE);
+            sendBroadcast(intent);
+        }else{
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    doStartApplicationWithPackageName(Constans.ALIPAY_PACKAGE);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    launchAlipayWallet();
+                }
+            }).start();
+        }
+    }
+
+    public boolean isRunning(){
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo rsi : am.getRunningServices(Integer.MAX_VALUE)) {
+            String pkgName = rsi.service.getPackageName();
+            if(pkgName.equals(Constans.ALIPAY_PACKAGE)){
+                if(rsi.process.equals(Constans.ALIPAY_PACKAGE)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void doStartApplicationWithPackageName(String packagename) {
+
+        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packagename = 参数packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // 设置ComponentName参数1:packagename参数2:MainActivity路径
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            startActivity(intent);
+        }
+    }
+
+    public static void wakeUpAndUnlock(Context context){
+        KeyguardManager km= (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock kl = km.newKeyguardLock("unLock");
+        //解锁
+        kl.disableKeyguard();
+        //获取电源管理器对象
+        PowerManager pm=(PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        //获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_DIM_WAKE_LOCK,"bright");
+        //点亮屏幕
+        wl.acquire();
+        //释放
+        wl.release();
     }
 
     public void finishWechatWallet() {

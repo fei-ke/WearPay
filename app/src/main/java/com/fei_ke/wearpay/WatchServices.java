facdc2c915072d11b32877d620a1c7288b5e70f3
fei-ke <jinyang656@qq.com>
2015-10-31 19:53:57 +0800

    启动支付宝简化

--- app/src/main/java/com/fei_ke/wearpay/WatchServices.java
+++ app/src/main/java/com/fei_ke/wearpay/WatchServices.java
@@ -2,15 +2,10 @@ package com.fei_ke.wearpay;
 
 import android.app.ActivityManager;
 import android.app.KeyguardManager;
-import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.os.IBinder;
 import android.os.PowerManager;
@@ -30,7 +25,6 @@ import com.google.android.gms.wearable.Wearable;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.List;
 
 import hugo.weaving.DebugLog;
 
@@ -166,7 +160,8 @@ public class WatchServices extends WearService {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    doStartApplicationWithPackageName(Constans.ALIPAY_PACKAGE);
+                    Intent intentForPackage = getPackageManager().getLaunchIntentForPackage(Constans.ALIPAY_PACKAGE);
+                    startActivity(intentForPackage);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -191,46 +186,6 @@ public class WatchServices extends WearService {
         return false;
     }
 
-    private void doStartApplicationWithPackageName(String packagename) {
-
-        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
-        PackageInfo packageinfo = null;
-        try {
-            packageinfo = getPackageManager().getPackageInfo(packagename, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageinfo == null) {
-            return;
-        }
-
-        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
-        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
-        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        resolveIntent.setPackage(packageinfo.packageName);
-
-        // 通过getPackageManager()的queryIntentActivities方法遍历
-        List<ResolveInfo> resolveinfoList = getPackageManager()
-                .queryIntentActivities(resolveIntent, 0);
-
-        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
-        if (resolveinfo != null) {
-            // packagename = 参数packname
-            String packageName = resolveinfo.activityInfo.packageName;
-            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
-            String className = resolveinfo.activityInfo.name;
-            // LAUNCHER Intent
-            Intent intent = new Intent(Constans.ACTION_LAUNCH_ALIPAY_WALLET);
-            intent.addCategory(Intent.CATEGORY_LAUNCHER);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            // 设置ComponentName参数1:packagename参数2:MainActivity路径
-            ComponentName cn = new ComponentName(packageName, className);
-
-            intent.setComponent(cn);
-            startActivity(intent);
-        }
-    }
 
     public static void wakeUpAndUnlock(Context context){
         KeyguardManager km= (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
